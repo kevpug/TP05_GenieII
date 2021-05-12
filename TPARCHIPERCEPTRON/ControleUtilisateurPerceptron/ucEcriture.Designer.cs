43189adc15c8f9cb26acccfff7d21519c890d036
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-05-11 22:39:42 -0400

    maybelinne

--- TPARCHIPERCEPTRON/ControleUtilisateurPerceptron/ucEcriture.Designer.cs
+++ TPARCHIPERCEPTRON/ControleUtilisateurPerceptron/ucEcriture.Designer.cs
@@ -29,18 +29,60 @@ namespace ControleUtilisateurPerceptron
         /// </summary>
         private void InitializeComponent()
         {
+            this.grpDessin = new System.Windows.Forms.GroupBox();
+            this.ucZoneDessin1 = new ControleUtilisateurPerceptron.ucZoneDessin();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.grpDessin.SuspendLayout();
             this.SuspendLayout();
             // 
-            // CUEcriture
+            // grpDessin
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.grpDessin.Controls.Add(this.ucZoneDessin1);
+            this.grpDessin.Location = new System.Drawing.Point(16, 19);
+            this.grpDessin.Name = "grpDessin";
+            this.grpDessin.Size = new System.Drawing.Size(129, 155);
+            this.grpDessin.TabIndex = 1;
+            this.grpDessin.TabStop = false;
+            this.grpDessin.Text = "Zone de dessin";
+            // 
+            // ucZoneDessin1
+            // 
+            this.ucZoneDessin1.BackColor = System.Drawing.Color.White;
+            this.ucZoneDessin1.Location = new System.Drawing.Point(20, 39);
+            this.ucZoneDessin1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.ucZoneDessin1.Name = "ucZoneDessin1";
+            this.ucZoneDessin1.Size = new System.Drawing.Size(85, 79);
+            this.ucZoneDessin1.TabIndex = 0;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(165, 19);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(182, 48);
+            this.btnOk.TabIndex = 2;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // ucEcriture
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "CUEcriture";
-            this.Size = new System.Drawing.Size(756, 444);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.grpDessin);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ucEcriture";
+            this.Size = new System.Drawing.Size(661, 304);
+            this.grpDessin.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private ucZoneDessin ucZoneDessin1;
+        private System.Windows.Forms.GroupBox grpDessin;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnOk;
     }
 }
--- TPARCHIPERCEPTRON/ControleUtilisateurPerceptron/ucEcriture.cs
+++ TPARCHIPERCEPTRON/ControleUtilisateurPerceptron/ucEcriture.cs
@@ -16,5 +16,6 @@ namespace ControleUtilisateurPerceptron
         {
             InitializeComponent();
         }
+
     }
 }
