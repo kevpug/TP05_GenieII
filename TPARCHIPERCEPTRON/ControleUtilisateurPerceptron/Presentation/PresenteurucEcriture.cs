b70d40c7879a3752839c074e17daad1ffda52437
Arnaud Labrecque <70404494+ArnaudLabTech@users.noreply.github.com>
2021-05-12 23:45:37 -0400

    Push i guess

---
+++ TPARCHIPERCEPTRON/ControleUtilisateurPerceptron/Presentation/PresenteurucEcriture.cs
@@ -0,0 +1,19 @@
+﻿using ControleUtilisateurPerceptron.Vue;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleUtilisateurPerceptron.Presentation
+{
+    public class PresenteurucEcriture
+    {
+        private IucEcriture _vue;
+        public PresenteurucEcriture(IucEcriture vue)
+        {
+            _vue = vue;
+           // _vue.EntrainementEvt += Entrainement;
+        }
+    }
+}
--- TPARCHIPERCEPTRON/ControleUtilisateurPerceptron/Vue/ucEcriture.cs
+++ TPARCHIPERCEPTRON/ControleUtilisateurPerceptron/Vue/ucEcriture.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ControleUtilisateurPerceptron.Presentation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,12 +11,36 @@ using System.Windows.Forms;
 
 namespace ControleUtilisateurPerceptron.Vue
 {
-    public partial class ucEcriture : UserControl
+    public partial class ucEcriture : UserControl, IucEcriture
     {
+        private string _cheminFichierEntrainement = "";
+        private bool _modePhrase;
+        private double _constanteApprentissage;
+        private PresenteurucEcriture _presentateur;
+
+
+        [Category("Parametre Ecriture")]
+        public string CheminFichierEntrainement 
+        { get { return _cheminFichierEntrainement; }
+            set { _cheminFichierEntrainement = value; }
+        }
+
+        [Category("Parametre Ecriture")]
+        public bool modePhrase 
+        { get { return _modePhrase; }
+            set { _modePhrase = value; } 
+        }
+
+        [Category("Parametre Ecriture")]
+        public double ConstanteApprentissage
+        { get { return _constanteApprentissage; }
+            set { _constanteApprentissage = value; } 
+        }
+
         public ucEcriture()
         {
             InitializeComponent();
+            _presentateur = new PresenteurucEcriture(this);
         }
-
     }
 }
